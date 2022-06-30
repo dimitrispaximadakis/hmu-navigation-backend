ee26697ac3c26cb5e328c08e8e608c4a6f190b77
Pax <pax@pop-os.localdomain>
2022-06-30 13:04:43 +0300

    heroku update

--- src/connection.js
+++ src/connection.js
@@ -2,11 +2,16 @@ const { Client } = require('pg')
 
 /* Connecting to the database using the needed credentials. */
 const client = new Client({
-    host: "localhost",
-    user: "postgres",
-    port: 5432,
-    password: "root",
-    database: "hmu"
-})
+    // host: "localhost",
+    // user: "postgres",
+    // port: 5432,
+    // password: "root",
+    // database: "hmu"
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
 
 module.exports = client
